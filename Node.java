99d606ab69e82f905131456596616d997531e2ca
Ashwin Vaithianathan <ashwinmelo12@gmail.com>
2013-03-06 12:47:07 -0700

    wrote node

--- Node.java
+++ Node.java
@@ -1,14 +1,51 @@
 /**
  * Node
  *
- * 		A member of a linked list. Holds an integer value and a link to another node.
+ * 		A member of a linked list. Holds an integer value 
+ * 		and a link to another node.
  */
 
 
-
-
-
-
 public class Node {
+
+	private int value;
+	private Node next;
+
+/**
+ * Constructors
+ */
+	public Node(int value, Node next) {
+		this.value = value;
+		this.next = next;
+	}
+
+	public Node() {
+		this(0, null);
+	}
+
+	public Node(int value) {
+		this(value, null);
+	}
 	
+
+/**
+ * Getters and Setters
+ */
+
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+
+
 }
