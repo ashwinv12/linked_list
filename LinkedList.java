2c44ac8ea877176354d5d94c4f1a4352e0518ebb
Ashwin Vaithianathan <ashwinmelo12@gmail.com>
2013-03-12 08:41:29 -0600

    created length and buildOneTwoThree methods

--- LinkedList.java
+++ LinkedList.java
@@ -6,7 +6,7 @@
 public class LinkedList {
 	
 	private Node head;
-
+	public int length = 0;
 
 	/**
 	 * Constructors
@@ -21,13 +21,44 @@ public class LinkedList {
 
 	/**
 	 * push
-	 *
+	 *		Given an int, add a new node at the head of the list with this value.
+	 *		
 	 * @param int value - push takes in the value to store in the new node
 	 */
 	public static void push(int value) {
+		length++;
 		Node n = new Node(value, null);
 		n.setNext(head.getNext());
 	}
 
-	
+
+	/**
+	 * length
+	 * 		returns the number of nodes in the linked list
+	 * @return length - number of nodes
+	 */
+	public static int length() {
+		return length;
+	}
+
+
+	/**
+	 * buildOneTwoThree
+	 *
+	 * 		Set the values of this list to {1, 2, 3}. Overwrite any previous values in the list. 
+	 * 		The length after this operation should be 3.
+	 * 
+	 */
+	public static void buildOneTwoThree() {
+		push(1);
+		push(2);
+		push(3);
+	}
+
+
 }
+
+
+
+
+
